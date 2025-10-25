2bfd0b5feba5a4676595d04531b1e675f991529e
Alex Lavigin <153559840+alex-l-iwpdev@users.noreply.github.com>
2025-10-25 15:23:43 +0200

    add thanks page template

---
+++ page-thanks.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Thanks, Page Template
+ *
+ * @package iwpdev/bulk-qr-theme
+ */
+
+get_header();
+?>
+	<section>
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col">
+					<div class="description">
+						<h2>Thank You for Your Payment!</h2>
+						<p>Your order has been successfully processed.</p>
+						<p>If you have any questions, feel free to <br>contact our support team at <a href="#">support@qr-bulk.com</a>
+						</p><a class="btn bi bi-house-door-fill" href="/">Return to Home Page</a>
+					</div>
+				</div>
+				<div class="col-auto">
+					<img
+							src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/thank-you.svg' ); ?>"
+							alt="Thanks, Page image">
+				</div>
+			</div>
+		</div>
+	</section>
+<?php
+get_footer();
