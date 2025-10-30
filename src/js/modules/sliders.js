4d5b854d4115a4ab840a4d9838af1cdc4c540827
on1989 <jenjalavigin@gmail.com>
2025-10-30 11:45:01 +0200

    fix menu

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,10 +1,11 @@
-jQuery( document ).ready( function( $ ) {
-	let reviewsSlider = $( '.reviews-items' ),
-		bannerSlider = $( '.banner-slider' ),
+jQuery(document).ready(function ($) {
+	let reviewsSlider = $('.reviews-items'),
+		bannerSlider = $('.banner-slider'),
+		planSlider = $('.plans-items'),
 		iconArrowPrev = '<i class="bi bi-chevron-left"></i>',
 		iconArrowNext = '<i class="bi bi-chevron-right"></i>';
-	if ( bannerSlider.length ) {
-		bannerSlider.slick( {
+	if (bannerSlider.length) {
+		bannerSlider.slick({
 			slidesToShow: 1,
 			prevArrow: iconArrowPrev,
 			nextArrow: iconArrowNext,
@@ -15,10 +16,10 @@ jQuery( document ).ready( function( $ ) {
 						arrows: false
 					}
 				}]
-		} );
+		});
 	}
-	if ( reviewsSlider.length ) {
-		reviewsSlider.slick( {
+	if (reviewsSlider.length) {
+		reviewsSlider.slick({
 			slidesToShow: 3,
 			slidesToScroll: 1,
 			arrows: false,
@@ -26,10 +27,8 @@ jQuery( document ).ready( function( $ ) {
 			infinite: true,
 			responsive: [
 				{
-					breakpoint: 1200,
-					settings: {
-						slidesToShow: 2
-					}
+					breakpoint: 992,
+					settings: "unslick"
 				},
 				{
 					breakpoint: 768,
@@ -38,6 +37,39 @@ jQuery( document ).ready( function( $ ) {
 					}
 				}
 			]
-		} );
+		});
 	}
-} );
+	if (planSlider.length) {
+		function slederPlan() {
+			if ($(window).width() >= 992) {
+				if (planSlider.hasClass('slick-initialized')) {
+					planSlider.slick('unslick');
+				}
+			} else if (!planSlider.hasClass('slick-initialized')) {
+				planSlider.slick({
+					arrows: false,
+					dots: true,
+					infinite: false,
+					responsive: [
+						{
+							breakpoint: 992,
+							settings: {
+								slidesToShow: 2
+							}
+						},
+						{
+							breakpoint: 768,
+							settings: {
+								slidesToShow: 1
+							}
+						}
+					]
+				});
+			}
+		}
+		slederPlan();
+		$(window).on('resize', function () {
+			slederPlan();
+		});
+	}
+});
