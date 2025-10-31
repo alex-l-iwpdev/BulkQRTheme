70f7f9cd56497dfbd5318cb4494f89636ecbdc60
Alex Lavigin <153559840+alex-l-iwpdev@users.noreply.github.com>
2025-10-31 09:50:41 +0100

    add page template

---
+++ page.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Page template.
+ *
+ * @package iwpdev/bulk-qr-theme
+ */
+
+get_header();
+if ( ! is_home() && ! is_front_page() ) {
+	?>
+	<section class="page-content">
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<h1><?php the_title(); ?></h1>
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+	</section>
+	<?php
+} else {
+	if ( have_posts() ) :
+		while ( have_posts() ) : the_post();
+			the_content();
+		endwhile;
+	endif;
+}
+get_footer();
