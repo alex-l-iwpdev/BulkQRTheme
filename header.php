cc38a5ef96703c9d618867d2d0e2728de05bf515
Alex Lavigin <153559840+alex-l-iwpdev@users.noreply.github.com>
2025-10-31 10:42:06 +0100

    add gtm

--- header.php
+++ header.php
@@ -20,6 +20,27 @@
 			href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&amp;display=swap"
 			rel="stylesheet">
 	<?php wp_head(); ?>
+	<!-- Google Tag Manager -->
+	<script>( function( w, d, s, l, i ) {
+			w[ l ] = w[ l ] || [];
+			w[ l ].push( {
+				'gtm.start':
+					new Date().getTime(), event: 'gtm.js'
+			} );
+			var f = d.getElementsByTagName( s )[ 0 ],
+				j = d.createElement( s ), dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore( j, f );
+		} )( window, document, 'script', 'dataLayer', 'GTM-T3TS9K7J' );</script>
+	<!-- End Google Tag Manager -->
+	<!-- Google Tag Manager (noscript) -->
+	<noscript>
+		<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3TS9K7J"
+				height="0" width="0" style="display:none;visibility:hidden"></iframe>
+	</noscript>
+	<!-- End Google Tag Manager (noscript) -->
 </head>
 <body <?php body_class(); ?>>
 <?php
